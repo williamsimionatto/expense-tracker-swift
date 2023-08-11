cb271d3e5b7cd41113520d97b17fb2b268113292
williamsimionatto <william.sn0226@gmail.com>
2023-08-11 13:17:52 -0300

    chore: init Swifit project

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by William Nepomuceno on 11/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
