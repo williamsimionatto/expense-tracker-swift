30abc6c12544aadefdaa03445740e0ab38bc7b92
williamsimionatto <william.sn0226@gmail.com>
2023-08-11 20:50:18 -0300

    feat: create overview page

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by William Nepomuceno on 11/08/23.
+//
+
+import Foundation
+
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
